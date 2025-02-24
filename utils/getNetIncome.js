3016ef58bab11d0b27e747550dd64e0f5bff3e67
peter <petersharp.dev>
2025-02-23 18:16:28 -0700

    Fixes getNetIncome incorrectly calculating tax rate

--- utils/getNetIncome.js
+++ utils/getNetIncome.js
@@ -1,5 +1,6 @@
 import percentOf from "./percentOf.js"
 export default function getNetIncome(duration, rate, tax) {
-    return duration * rate - percentOf(tax, rate)
+    const gross = duration * rate;
+    return gross - percentOf(tax, gross);
 }
 
--- utils/percentOf.js
+++ utils/percentOf.js
@@ -1,4 +1,4 @@
 
 export default function percentOf(percent, number) {
-    return percent/100 * number
+    return percent / 100 * number
 }
