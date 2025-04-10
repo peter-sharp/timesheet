391d2651da30b7ad959002ff8780b1710ad776c7
peter <petersharp.dev>
2025-04-09 21:50:15 -0600

    Enhances task archive  by integrating signal, context state management for archive data, including pagination and search capabilities. Updates event handling for improved user interaction and data rendering.

--- app-context.js
+++ app-context.js
@@ -9,6 +9,10 @@ customElements.define('app-context', class extends HTMLElement {
     durationTotal = signal(0)
     durationTotalGaps = signal(0)
     currentTask = signal({})
+    archive = signal({ tasks: [], entries: [] })
+    archiveOpen = signal(false)
+    archiveBrowserTaskPage = signal(0)
+    archiveBrowserTaskPageSize = signal(20)
     stateProvider = new ContextProvider(this, 'state', {
         settings: this.settings,
         newEntry: this.newEntry,
@@ -17,7 +21,11 @@ customElements.define('app-context', class extends HTMLElement {
         clients: this.clients,
         durationTotal: this.durationTotal,
         durationTotalGaps: this.durationTotalGaps,
-        currentTask: this.currentTask
+        currentTask: this.currentTask,
+        archive: this.archive,
+        archiveOpen: this.archiveOpen,
+        archiveBrowserTaskPage: this.archiveBrowserTaskPage,
+        archiveBrowserTaskPageSize: this.archiveBrowserTaskPageSize
     });
     updateProvider = new ContextProvider(this, 'state-update', ({...state}) => {
         this.stateProvider.value = {...state};
@@ -26,7 +34,7 @@ customElements.define('app-context', class extends HTMLElement {
         this.style.display = 'contents';
     }
 
-    update({newEntry, entries, tasks, clients, settings, durationTotal, durationTotalGaps, currentTask}) {
+    update({newEntry, entries, tasks, clients, settings, durationTotal, durationTotalGaps, currentTask, archive, archiveOpen, archiveBrowserTaskPage, archiveBrowserTaskPageSize}) {
         this.settings.value = {...settings}
         this.newEntry.value = {...newEntry}
         this.entries.value = [...entries]
@@ -35,5 +43,9 @@ customElements.define('app-context', class extends HTMLElement {
         this.durationTotal.value = durationTotal
         this.durationTotalGaps.value = durationTotalGaps
         this.currentTask.value = currentTask
+        this.archive.value = {...archive}
+        this.archiveOpen.value = archiveOpen
+        this.archiveBrowserTaskPage.value = archiveBrowserTaskPage
+        this.archiveBrowserTaskPageSize.value = archiveBrowserTaskPageSize
     }
 });
--- archive/task-archive.js
+++ archive/task-archive.js
@@ -1,5 +1,8 @@
 import newtemplateItem from "../utils/newTemplateItem.js";
 import emitEvent from "../utils/emitEvent.js";
+import { ContextRequestEvent } from "../utils/Context.js";
+import { effect } from "../utils/Signal.js";
+
 const template = document.createElement('template');
 template.innerHTML = /*html*/`
 <form class="input-group row">
@@ -10,12 +13,11 @@ template.innerHTML = /*html*/`
 <ul id="archive_tasks_list" class="unstyled-list stack"></ul>
 <nav class="pagination overflow-x-scroll">
 <ol  id="archive_tasks_page_nav"></ol>
-</nav>`
+</nav>`;
 
 const archiveTasksPageNavItem = document.createElement('template');
 archiveTasksPageNavItem.innerHTML = /*html*/`
-        <li aria-selected="true"><button type="button" data-style="subtle"></button></li>
-`
+        <li aria-selected="true"><button type="button" data-style="subtle"></button></li>`;
 
 const archiveTaskItem = document.createElement('template');
 archiveTaskItem.innerHTML = /*html*/`
@@ -30,45 +32,85 @@ archiveTaskItem.innerHTML = /*html*/`
         </p>
         <p class="task-item__description" data-description></p>
     </div>
-</li>`
+</li>`;
 
 class TaskArchive extends HTMLElement {
-    archivedTasksSearchTerm = ""
+    #archive;
+    #archiveOpen;
+    #archiveBrowserTaskPage;
+    #archiveBrowserTaskPageSize;
+    #unsubscribe = {};
+    archivedTasksSearchTerm = "";
+
     constructor() {
         super();
         this.append(template.content.cloneNode(true));
         const el = this;
 
-        const elArchiveTasksNav = el.querySelector("#archive_tasks_page_nav")
+        const elArchiveTasksNav = el.querySelector("#archive_tasks_page_nav");
         const tasksList = el.querySelector('#archive_tasks_list');
 
         elArchiveTasksNav.addEventListener("click", function updatePage(ev) {
-            if(ev.target.nodeName.toLowerCase() == "button") {
+            if (ev.target.nodeName.toLowerCase() == "button") {
                 emitEvent(el, "updateArchiveTaskPage", {
                     page: parseInt(ev.target.innerText, 10)
-                })
+                });
             }
         });
 
         this.searchForm = el.querySelector("form");
-        this.searchForm.addEventListener("submit", searchArchive.bind(this));
-        function searchArchive(ev) {
-            ev.preventDefault();
-            this.archivedTasksSearchTerm = this.searchForm.elements.term.value;
-            this.render({ ...this.state, archivedTasksSearchTerm: this.archivedTasksSearchTerm, archiveBrowserTaskPage: 0 });
-        }
+        this.searchForm.addEventListener("submit", this.searchArchive.bind(this));
 
         this.tasksList = tasksList;
         this.elArchiveTasksNav = elArchiveTasksNav;
+
+        this.dispatchEvent(
+            new ContextRequestEvent(
+                "state",
+                (state, unsubscribe) => {
+                    this.#archive = state.archive;
+                    this.#archiveOpen = state.archiveOpen;
+                    this.#archiveBrowserTaskPage = state.archiveBrowserTaskPage;
+                    this.#archiveBrowserTaskPageSize = state.archiveBrowserTaskPageSize;
+
+                    this.#unsubscribe.signals = effect(
+                        this.update.bind(this),
+                        this.#archive,
+                        this.#archiveOpen,
+                        this.#archiveBrowserTaskPage,
+                        this.#archiveBrowserTaskPageSize
+                    );
+
+                    this.#unsubscribe.state = unsubscribe;
+                },
+                true
+            )
+        );
     }
 
-    update(state) {
-        this.state = state;
-        this.render({...this.state, archivedTasksSearchTerm: this.archivedTasksSearchTerm});
+    disconnectedCallback() {
+        this.#unsubscribe.signals();
+        this.#unsubscribe.state();
     }
-    
-    render({ archiveOpen, archive = { tasks: [] }, archivedTasksSearchTerm=null, archiveBrowserTaskPage = 0, archiveBrowserTaskPageSize = 20 }) {
-        if(!archiveOpen) return;
+
+    searchArchive(ev) {
+        ev.preventDefault();
+        this.archivedTasksSearchTerm = this.searchForm.elements.term.value;
+        this.update();
+    }
+
+    update() {
+        this.render({
+            archiveOpen: this.#archiveOpen?.value,
+            archive: this.#archive?.value || { tasks: [] },
+            archiveBrowserTaskPage: this.#archiveBrowserTaskPage?.value || 0,
+            archiveBrowserTaskPageSize: this.#archiveBrowserTaskPageSize?.value || 20,
+            archivedTasksSearchTerm: this.archivedTasksSearchTerm
+        });
+    }
+
+    render({ archiveOpen, archive, archivedTasksSearchTerm = null, archiveBrowserTaskPage = 0, archiveBrowserTaskPageSize = 20 }) {
+        if (!archiveOpen) return;
         const { elArchiveTasksNav } = this;
         const filteredTasks = archivedTasksSearchTerm ? archive.tasks.filter(this.filterBySearchTerm(archivedTasksSearchTerm)) : archive.tasks;
         const offset = archiveBrowserTaskPage * archiveBrowserTaskPageSize;
@@ -76,26 +118,25 @@ class TaskArchive extends HTMLElement {
         const toRender = [];
         for (let i = offset; i < lastIndex; i += 1) {
             toRender.push(filteredTasks[i]);
-        } 
-        console.log("tasks", {toRender, archiveBrowserTaskPage, archiveBrowserTaskPageSize})
+        }
         this.renderTasks(toRender);
 
         const pageCount = Math.ceil(filteredTasks.length / archiveBrowserTaskPageSize);
         const pages = document.createDocumentFragment();
         for (let i = 0; i < pageCount; i += 1) {
-            pages.append(this.renderPageNavItem({ pageNo: i, selectedPage: archiveBrowserTaskPage }))
-        } 
+            pages.append(this.renderPageNavItem({ pageNo: i, selectedPage: archiveBrowserTaskPage }));
+        }
 
-        elArchiveTasksNav.innerHTML = ""
-        elArchiveTasksNav.append(pages)
+        elArchiveTasksNav.innerHTML = "";
+        elArchiveTasksNav.append(pages);
     }
 
     filterBySearchTerm(term) {
-        return function Search({ exid, description, client}){
-            return (exid || '').toString().toLowerCase().includes(term) 
-                 || (description|| '').toLowerCase().includes(term) 
-                 || (client|| '').toLowerCase().includes(term)
-        }
+        return function Search({ exid, description, client }) {
+            return (exid || '').toString().toLowerCase().includes(term)
+                || (description || '').toLowerCase().includes(term)
+                || (client || '').toLowerCase().includes(term);
+        };
     }
 
     renderTasks(tasks) {
@@ -115,10 +156,10 @@ class TaskArchive extends HTMLElement {
     }
 
     renderPageNavItem({ pageNo, selectedPage }) {
-        const item = newtemplateItem(archiveTasksPageNavItem)
-        item.setAttribute('aria-selected', pageNo == selectedPage )
-        item.querySelector("button").innerText = pageNo
-        return item
+        const item = newtemplateItem(archiveTasksPageNavItem);
+        item.setAttribute('aria-selected', pageNo == selectedPage);
+        item.querySelector("button").innerText = pageNo;
+        return item;
     }
 }
 
